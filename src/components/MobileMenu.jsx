2226bfcbc51a6061909bcdc592cc98967f97bf86
Andrew Gluck <andrew.gluck91@gmail.com>
2025-03-05 14:31:00 -0500

    update modbile menu icon and z index

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -49,35 +49,9 @@ const MobileSidebar = () => {
         aria-label="Toggle mobile menu"
       >
         {isOpen ? (
-          <svg
-            className="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M6 18L18 6M6 6l12 12"
-            ></path>
-          </svg>
+          <i className="fa-solid fa-xmark w-5 h-5"></i>
         ) : (
-          <svg
-            className="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            ></path>
-          </svg>
+          <i className="fa-solid fa-bars w-5 h-5"></i>
         )}
       </button>
 
@@ -91,7 +65,7 @@ const MobileSidebar = () => {
 
       {/* Sidebar */}
       <div
-        className={`fixed top-0 right-0 w-64 h-full bg-white shadow-lg z-50 transform transition-transform duration-300 ease-in-out lg:hidden ${
+        className={`fixed top-0 right-0 w-64 h-full bg-white shadow-lg z-40 transform transition-transform duration-300 ease-in-out lg:hidden ${
           isOpen ? "translate-x-0" : "translate-x-full"
         }`}
       >
