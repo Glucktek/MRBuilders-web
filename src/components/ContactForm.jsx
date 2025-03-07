19a7a95ffd05257e31ac541684f896d4b584d027
Andrew Gluck <andrew.gluck91@gmail.com>
2025-03-07 00:22:03 -0500

    adding netlify support to form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -34,7 +34,7 @@ const ContactForm = () => {
           </div>
           {/* Right Section */}
           <div className="p-8 sm:p-12">
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit} netlify>
               <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
                 <div>
                   <label
