e9d72891e8f944365200633617ab5c71e744d901
Andrew Gluck <andrew.gluck91@gmail.com>
2025-03-07 00:35:42 -0500

    form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -27,7 +27,7 @@ const ContactForm = () => {
           </div>
           {/* Right Section */}
           <div className="p-8 sm:p-12">
-            <form name="contact" method="POST" netlify>
+            <form name="contact" netlify>
               <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
                 <div>
                   <label
